3856b30d69fe0e0aed439e89a80caec27d979c39
Shivam Chaudhary <84712785+shivam464@users.noreply.github.com>
2022-12-23 11:50:58 +0530

    added:dynamic product details

--- js/index.js
+++ js/index.js
@@ -1,6 +1,43 @@
-const hamburger = document.querySelector('.hamburger');
-const navLink = document.querySelector('.nav__link');
+// const hamburger = document.querySelector('.hamburger');
+// const navLink = document.querySelector('.nav__link');
 
-hamburger.addEventListener('click', () => {
-  navLink.classList.toggle('hide');
-});
+// hamburger.addEventListener('click', () => {
+//   navLink.classList.toggle('hide');
+// });
+
+
+
+const getResponse = async () => {
+
+  const resp = await fetch(
+    `https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=100&sort_by=&sort_dir=desc&filter=`
+  ).then((res) => res.json());
+  const response = resp.result;
+  console.log("response", response);
+  show(response);
+}
+
+function show(data) {
+
+  let html = '';
+  data.products.forEach(product => {
+    let htmlSegment = `<div class="card">
+                        <img src="${product.image}" >  
+                        <div class="product-details">
+                        <p class="product-name">${product.name}</p>
+                        <p class="product-price">Rs.${product.price}</p>
+                        <div class="heart-icon"><i class="fa-regular fa-heart"></i></div>
+                        </div>
+                    </div>`;
+
+    html += htmlSegment;
+  });
+
+  let cardcontainer = document.querySelector(".cards")
+  cardcontainer.innerHTML = html;
+}
+
+
+
+getResponse();
+
