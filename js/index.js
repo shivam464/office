49c990ef264b27bc0adb1c08b9984ac6e7e8867c
Shivam Chaudhary <84712785+shivam464@users.noreply.github.com>
2022-12-26 18:00:45 +0530

    added:navbar

--- js/index.js
+++ js/index.js
@@ -10,14 +10,60 @@
 const getResponse = async () => {
 
   const resp = await fetch(
-    `https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=100&sort_by=&sort_dir=desc&filter=`
+    `https://pim.wforwoman.com/pim/pimresponse.php/?service=category&store=1&url_key=top-wear-kurtas&page=1&count=20&sort_by=&sort_dir=desc&filter=`
   ).then((res) => res.json());
   const response = resp.result;
   console.log("response", response);
   show(response);
 }
 
+function abc(data) {
+  return `
+    <div class="content">
+       <label class="container">${filter.filter_lable}
+        input type="checkbox" checked="checked">
+        <span class="checkmark"></span>
+       </label>
+     </div>
+`
+}
+
 function show(data) {
+  let filters = "";
+  data.filters.forEach(filter => {
+    // console.log("filter", filter);
+
+
+    let filtersegment = `<div class="collapsible-wrapper">
+      <p>${filter.filter_lable}</p>
+    </div>
+    `
+    // const card = document.createElement('div');
+    // card.classList.add('content');
+
+    // filter.options.forEach(Option => {
+    //   card.innerHTML = `
+    //      <label class="container">${Option.value}
+    //        <input type="checkbox" checked="checked">
+    //        <span class="checkmark"></span>
+    //      </label>`
+    //      data+=card
+    // })
+
+
+
+    //   let filtersegment = `<div class="content">
+    //     <label class="container">${filter.filter_lable}
+    //     <input type="checkbox" checked="checked">
+    //     <span class="checkmark"></span>
+    //   </label>
+    // </div>`;
+    filters += filtersegment;
+  })
+  let filtercontainer = document.querySelector(".left-container")
+  console.log('filtercontainer', filtercontainer);
+  filtercontainer.innerHTML = filters;
+
 
   let html = '';
   data.products.forEach(product => {
@@ -36,8 +82,20 @@ function show(data) {
   let cardcontainer = document.querySelector(".cards")
   cardcontainer.innerHTML = html;
 }
-
-
-
 getResponse();
 
+var coll = document.getElementsByClassName("collapsible-wrapper");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function () {
+    console.log("hyy", coll)
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
+}
